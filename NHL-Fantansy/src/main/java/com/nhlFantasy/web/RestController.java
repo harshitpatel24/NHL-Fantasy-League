1acd404dd38b53e4fdf0416df030bf2ddfa1e065
Harshitkumar Patel <harshitpatel24@gmail.com>
2018-11-01 14:37:17 -0400

    added and integrated demo-api-client with spring boot

--- NHL-Fantansy/src/main/java/com/nhlFantasy/web/RestController.java
+++ NHL-Fantansy/src/main/java/com/nhlFantasy/web/RestController.java
@@ -1,13 +1,12 @@
 package com.nhlFantasy.web;
-
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.boot.SpringApplication;
+
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
 	@RequestMapping(value = "/api/hello")
 	 public String greet() {
-	  return "Hello from the other side!!!";
+	  return "Hello from the other side124342343!!!";
 	 }
 }
