ac53c5784dd1f7155800b984c1b27bc6be08828d
Prateeti Deb Chaudhuri <prateetidebchaudhuri@v1020-wn-34-234.campus-dynamic.uwaterloo.ca>
2018-11-13 22:28:17 -0500

    Implemented League service (back-end)

---
+++ NHL-Fantansy/src/main/java/com/nhlFantasy/service/LeagueService.java
@@ -0,0 +1,8 @@
+package com.nhlFantasy.service;
+
+import com.nhlFantasy.entity.League;
+
+public interface LeagueService {
+
+	League addLeague(League league);
+}
---
+++ NHL-Fantansy/src/main/java/com/nhlFantasy/serviceImpl/LeagueServiceImpl.java
@@ -0,0 +1,21 @@
+package com.nhlFantasy.serviceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nhlFantasy.dao.LeagueRepository;
+import com.nhlFantasy.entity.League;
+import com.nhlFantasy.service.LeagueService;
+
+@Service("LeagueService")
+public class LeagueServiceImpl implements LeagueService{
+
+	@Autowired
+	LeagueRepository leagueRepository;
+	@Override
+	public League addLeague(League league) {
+		// TODO Auto-generated method stub
+		return leagueRepository.save(league);
+	}
+
+}
