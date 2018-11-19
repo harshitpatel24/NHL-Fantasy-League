9f883aa07a317261d62abf12a366d7d2a8c66a8f
Prateeti Deb Chaudhuri <prateetidebchaudhuri@v1020-wn-215-215.campus-dynamic.uwaterloo.ca>
2018-11-19 16:10:21 -0500

    changed to jsonnode

--- NHL-Fantansy/src/main/java/com/nhlFantasy/web/LeagueController.java
+++ NHL-Fantansy/src/main/java/com/nhlFantasy/web/LeagueController.java
@@ -14,7 +14,8 @@ import com.nhlFantasy.dao.UserRepository;
 import com.nhlFantasy.entity.League;
 import com.nhlFantasy.entity.User;
 import com.nhlFantasy.service.LeagueService;
-import com.fasterxml.jackson.databind.node.ObjectNode;;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 @RestController
 public class LeagueController {
 
@@ -23,17 +24,9 @@ public class LeagueController {
 	UserRepository userRepository;
 	
 	@RequestMapping(value = "/api/addLeague", method = RequestMethod.POST,consumes = "application/json", produces = "application/json")
-	public @ResponseBody League addLeague(@RequestBody ObjectNode objNode,  HttpServletResponse response, HttpServletRequest  request) {
-
-		//int id = 5;
-		//User u = new User();
-		//u.setUserid(5);
-		//u.setUname("test");
-		//u.setEmail("demo@gmail.com");
-		//u.setPassword("test");
-		//User u = userRepository.searchUserbyId(id);
-		//league.setUser(u);
-		//System.out.println(league.getUser());
+	public @ResponseBody JsonNode addLeague(@RequestBody JsonNode objNode,  HttpServletResponse response, HttpServletRequest  request) {
+		
+		ObjectMapper mapper = new ObjectMapper();
 		User user = new User();
 		user.setUserid(Integer.parseInt(objNode.get("creatorId").get("userid").toString()));
 		League league = new League();
@@ -41,11 +34,11 @@ public class LeagueController {
 		league.setLeagueName(objNode.get("leagueName").toString());
 		league.setLeaguePassword(objNode.get("leaguePassword").toString());
 		league.setUser(user);
-		//System.out.println(objNode.get("leaguePassword").toString());
-	
-		//return null;
-		League leagueObjForSave = leagueService.addLeague(league);
 		
-		return leagueObjForSave;
+		League leagueObjForSave = leagueService.addLeague(league);
+		JsonNode node = mapper.convertValue(leagueObjForSave, JsonNode.class);
+		
+		
+		return node;
 	}
 }
