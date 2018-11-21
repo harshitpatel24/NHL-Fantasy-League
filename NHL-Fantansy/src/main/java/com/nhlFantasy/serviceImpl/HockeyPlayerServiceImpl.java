95598639dd56e5d89f2309c13c1231ce269d5411
Prateeti Deb Chaudhuri <prateetidebchaudhuri@v1020-wn-215-215.campus-dynamic.uwaterloo.ca>
2018-11-20 21:19:49 -0500

    committing player changes

---
+++ NHL-Fantansy/src/main/java/com/nhlFantasy/serviceImpl/HockeyPlayerServiceImpl.java
@@ -0,0 +1,26 @@
+package com.nhlFantasy.serviceImpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nhlFantasy.dao.HockeyPlayerRepository;
+import com.nhlFantasy.entity.HockeyPlayer;
+import com.nhlFantasy.service.HockeyPlayerService;
+
+@Service
+public class HockeyPlayerServiceImpl implements HockeyPlayerService{
+
+	@Autowired
+	HockeyPlayerRepository hockeyPlayerRepository;
+	@Override
+	public List<HockeyPlayer> findAllHockeyPlayerByTeam(String teamName) {
+		// TODO Auto-generated method stub
+		//List<HockeyPlayer> listofHock = hockeyPlayerRepository.findAllHockeyPlayersByTeam(teamName);
+		//System.out.println("1111" + listofHock.size());
+		
+		return hockeyPlayerRepository.findAllHockeyPlayersByTeam(teamName);
+	}
+
+}
---
+++ NHL-Fantansy/src/main/java/com/nhlFantasy/web/HockeyPlayerController.java
@@ -0,0 +1,44 @@
+package com.nhlFantasy.web;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhlFantasy.entity.HockeyPlayer;
+import com.nhlFantasy.service.HockeyPlayerService;
+
+@RestController
+public class HockeyPlayerController {
+	
+	@Autowired
+	HockeyPlayerService hockeyPlayerService;
+
+	@RequestMapping(value = "/api/getAllPlayersbyTeam", method = RequestMethod.POST,consumes = "application/json", produces = "application/json")
+	public @ResponseBody JsonNode getAllHockeyPlayersByTeam(@RequestBody JsonNode objNode,  HttpServletResponse response, HttpServletRequest  request) {
+	
+		ObjectMapper mapper = new ObjectMapper();
+		
+		String teamName = objNode.get("teamName").toString();
+		teamName = teamName.substring(1, teamName.length()- 1);
+		System.out.println("team name = "+ teamName);
+		
+		List<HockeyPlayer> hockeyPlayers = hockeyPlayerService.findAllHockeyPlayerByTeam(teamName);
+		
+		//System.out.println("list of hockey players = " + hockeyPlayers.size());
+		JsonNode node = null;
+		
+		node = mapper.convertValue(hockeyPlayers, JsonNode.class);
+		
+		return node;
+	}
+}
