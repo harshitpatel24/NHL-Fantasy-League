ed7185a93a1980dfa2b18999a99001785e3bdf14
Harshitkumar Patel <harshitpatel24@gmail.com>
2018-11-29 21:31:14 -0400

    resolved pointStatus query

--- NHL-Fantansy/src/main/java/com/nhlFantasy/dao/HockeyPlayerStatsArchiveRepository.java
+++ NHL-Fantansy/src/main/java/com/nhlFantasy/dao/HockeyPlayerStatsArchiveRepository.java
@@ -10,7 +10,7 @@ import com.nhlFantasy.entity.HockeyPlayerStatsArchive;;
 
 public interface HockeyPlayerStatsArchiveRepository extends JpaRepository<HockeyPlayerStatsArchive, Long>{
 	
-	@Query(nativeQuery = true, value = "select * from hockeyPlayerStatsArchive where playerId in(select playerId from hockeyPlayer where playerId in (select playerId from memberTeam where memberId in (select id from leagueMember where userid = :userid and leagueId = :leagueid)))")
+	@Query(nativeQuery = true, value = "select * from hockeyPlayerStatsArchive where playerId in(select playerId from hockeyPlayer where playerId in (select playerId from memberTeamArchive where memberId in (select id from leagueMember where userid = :userid and leagueId = :leagueid)))")
 	List<HockeyPlayerStatsArchive> getPointHistory(@Param("leagueid") int leagueId,@Param("userid") int userid);
 
 }
